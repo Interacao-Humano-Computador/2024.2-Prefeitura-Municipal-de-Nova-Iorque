a4232fd972e585b1c305b262dbbee5d1e900623d
vevetin <mr.weverton1226@gmail.com>
2024-11-30 12:32:53 -0300

    chore: altera a baseUrl em docusaurus.config.js

--- Docs/docusaurus.config.js
+++ Docs/docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://Interacao-Humano-Computador.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/2024.2-Grupo06/',
+  baseUrl: '/2024.2-Prefeitura-Municipal-de-Nova-Iorque/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
